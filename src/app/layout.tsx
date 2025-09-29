883f3269d8b232369c68e6fc316e4a278e8345f6
George Nick Gorzynski <georgegorzynski@me.com>
2025-09-30 00:33:51 +0100

    ui: Add disclaimer card at bottom of page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import DisclaimerCard from "@/components/disclaimer-card";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
@@ -35,6 +36,7 @@ export default function RootLayout({
           >
             <div className="grid grid-cols-[auto_1fr] gap-4 p-4">
               {children}
+              <DisclaimerCard />
             </div>
           </ThemeProvider>
         </body>
---
+++ src/components/disclaimer-card.tsx
@@ -0,0 +1,11 @@
+import { Card, CardHeader, CardTitle } from "./ui/card";
+
+export default function DisclaimerCard() {
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>This application is not affiliated with TeamSport.</CardTitle>
+            </CardHeader>
+        </Card>
+    )
+}
