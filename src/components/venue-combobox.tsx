329cb02c75b80cbf93f21d89f65cf520f5d1dd72
George Nick Gorzynski <georgegorzynski@me.com>
2025-09-30 01:40:46 +0100

    ui: Change venue combobox alignment and width

--- src/components/venue-combobox.tsx
+++ src/components/venue-combobox.tsx
@@ -58,7 +58,7 @@ export function VenueSelector({
             <ChevronsUpDownIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
           </Button>
         </PopoverTrigger>
-        <PopoverContent className="w-[200px] p-0">
+        <PopoverContent className="w-[300px] p-0" align="start">
           <Command>
             <CommandInput placeholder="Search venues..." />
             <CommandList>
