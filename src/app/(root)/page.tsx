e2e9fa910b3041432262e08a490bbeae9bef18ac
George Nick Gorzynski <georgegorzynski@me.com>
2025-09-30 01:45:06 +0100

    planning: Add comments as placeholders for what to add here

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -4,6 +4,8 @@ export default function VenueSelectorPage() {
   return (
     <div className="flex flex-col min-h-screen">
       <Filters />
+      {/* Ranking table */}
+      {/* Kart-to-Kart comparison */}
     </div>
   )
 }
