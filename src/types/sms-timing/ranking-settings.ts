e6ead7bcddedb44d0965216173fb4a7eaada2cb2
George Nick Gorzynski <georgegorzynski@me.com>
2025-10-06 00:09:50 +0100

    fix: improve typing to reflect API responses

--- src/types/sms-timing/ranking-settings.ts
+++ src/types/sms-timing/ranking-settings.ts
@@ -1,6 +1,7 @@
-export type SmsTimingRakingSettings = {
+export type SmsTimingRankingSettings = {
     resources: Array<{
         id: string; name: string; resourceKind?: number;
-        scoregroups: Array<{ id: string; name: string }>;
+        scoregroups?: Array<{ id: string; name: string }>;
+        scoreGroups?: Array<{ id: string; name: string }>;
     }>;
 };
