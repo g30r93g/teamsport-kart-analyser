443b4fc5535560aac54efb32b17498189e55d875
George Nick Gorzynski <georgegorzynski@me.com>
2025-10-06 00:09:19 +0100

    func: add `dateToStartOfDay` util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,12 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function dateToStartOfDay(date: Date): Date {
+  const newDate = new Date(date);
+  newDate.setHours(0, 0, 0, 0);
+  return newDate;
 }
